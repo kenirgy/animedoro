6166e6fc0ac6249de1542a4753182ad197da57da
bartick <bartickmaiti1998@gmail.com>
2021-11-23 21:40:08 +0530

    Added tailwind css for furthur upgrades

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./pages/**/*.js'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
